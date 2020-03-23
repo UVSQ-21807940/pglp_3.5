b3d4363d612adaa423cbfde15f7bb81d4f2682ea
UVSQ-21807940 <37400158+UVSQ-21807940@users.noreply.github.com>
2020-03-24 00:06:50 +0100

    ajout class metier

---
+++ src/main/java/uvsq21807940/ClassMetier.java
@@ -0,0 +1,13 @@
+package uvsq21807940;
+
+import java.time.LocalDateTime;
+
+public class ClassMetier {
+	public void uneMethodeMetier(LogInter log) {
+		log.write(LocalDateTime.now() + "Debut methode métier");
+		
+    
+		log.write(LocalDateTime.now() + "Fin methode métier");
+	}
+
+}
