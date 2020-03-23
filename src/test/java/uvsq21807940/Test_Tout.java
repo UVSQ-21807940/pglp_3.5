64e4b9fca98f33ba32c86ff183c5025562d81f11
UVSQ-21807940 <37400158+UVSQ-21807940@users.noreply.github.com>
2020-03-24 00:10:00 +0100

    ajout test

---
+++ src/test/java/uvsq21807940/Test_Tout.java
@@ -0,0 +1,18 @@
+package uvsq21807940;
+
+import org.junit.Test;
+
+public class Test_Tout {
+
+	
+	@Test
+	public void dip_test() 
+	{
+		Log l = new Log();
+		ClassMetier m = new ClassMetier();
+		
+		m.uneMethodeMetier(l);
+		
+	}
+
+}
